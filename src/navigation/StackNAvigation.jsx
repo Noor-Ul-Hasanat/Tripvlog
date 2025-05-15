5cbf0b065847d0c9fd79cfb702a439dbb5dec7aa
Noor ul Hasanat <noorulhasanat123@gmail.com>
2025-05-15 18:17:32 +0500

    first commit

---
+++ src/navigation/StackNAvigation.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { Boarding } from '../screens/Boarding';
+// import { Home } from '../screens/Home';
+
+// export const StackNAvigation = () => {
+//   return (
+//  <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Boarding" screenOptions={{ headerShown: false }}>
+//         <Stack.Screen name="Boarding" component={Boarding} />
+//         <Stack.Screen name="Home" component={Home} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
---
+++ src/screens/Boarding.jsx
@@ -0,0 +1,59 @@
+// screens/BoardingScreen.js
+import React from 'react';
+import {View,Text,  StyleSheet, ImageBackground, TouchableOpacity} from 'react-native';
+export  const Boarding = ({ navigation })=> {
+  return (
+    <ImageBackground
+      source={require('../assests/OnBoarding.jpg')}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={styles.overlay}>
+        <Text style={styles.title}>Press play on your{'\n'}next adventure!</Text>
+        <Text style={styles.subtitle}>
+          Discover, book, and explore{'\n'}new destinations for adventure.
+        </Text>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('Home')}
+        >
+          <Text style={styles.buttonText}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  overlay: {
+    padding: 30,
+    backgroundColor: 'rgba(0,0,0,0.3)',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#00e676',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: 'white',
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: '#00e676',
+    paddingVertical: 12,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
